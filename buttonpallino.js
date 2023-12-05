8edd2c6701555ccf02a13ed0209e9fa9611239d5
MiriamPecora <miriampecora97@gmail.com>
2023-12-05 15:36:07 +0100

    Conclusa prima parte dell'esercizio sul calcolatore

--- buttonpallino.js
+++ buttonpallino.js
@@ -8,6 +8,26 @@
 //   Pallino.addEventListener("click", handleClick);
 // });
 
-document.getElementById("Pallino").addEventListener("click", function () {
-  console.log("Sono un pallino nero!");
+// document.getElementById("Pallino").addEventListener("click", function () {
+//   console.log("Sono un pallino nero!");
+// });
+
+contatoreElement = document.getElementById("contatore");
+incrementaButton = document.getElementById("incrementa");
+decrementaButton = document.getElementById("decrementa");
+
+incrementaButton.addEventListener("click", function () {
+  // Incrementare il valore del contatore
+  contatoreElement.textContent = +contatoreElement.textContent + 1;
+});
+
+decrementaButton.addEventListener("click", function () {
+  // Verifico che il valore sia maggiore di 0 prima di decrementare
+  if (+contatoreElement.textContent > 0) {
+    // Decrementare il valore del contatore
+    contatoreElement.textContent = +contatoreElement.textContent - 1;
+  } else {
+    // Se il valore è già 0, mostra un alert
+    alert("Il valore non può essere inferiore a 0!");
+  }
 });
