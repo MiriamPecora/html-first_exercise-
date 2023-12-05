17425020822aea0fe5d11143e9967f41aac852c3
MiriamPecora <miriampecora97@gmail.com>
2023-12-05 16:55:43 +0100

    Iniziato terzo esercizio e messo tra commenti gli esercizi precedenti

--- buttonpallino.js
+++ buttonpallino.js
@@ -1,3 +1,4 @@
+// primo esercizio:
 // document.addEventListener("DOMContentLoaded", function () {
 //   function handleClick() {
 //     console.log("Sono un pallino nero!");
@@ -12,22 +13,39 @@
 //   console.log("Sono un pallino nero!");
 // });
 
-contatoreElement = document.getElementById("contatore");
-incrementaButton = document.getElementById("incrementa");
-decrementaButton = document.getElementById("decrementa");
+// secondo esercizio:
+// contatoreElement = document.getElementById("contatore");
+// incrementaButton = document.getElementById("incrementa");
+// decrementaButton = document.getElementById("decrementa");
 
-incrementaButton.addEventListener("click", function () {
-  // Incrementare il valore del contatore
-  contatoreElement.textContent = +contatoreElement.textContent + 1;
-});
+// incrementaButton.addEventListener("click", function () {
+//   // Incrementare il valore del contatore
+//   contatoreElement.textContent = +contatoreElement.textContent + 1;
+// });
 
-decrementaButton.addEventListener("click", function () {
-  // Verifico che il valore sia maggiore di 0 prima di decrementare
-  if (+contatoreElement.textContent > 0) {
-    // Decrementare il valore del contatore
-    contatoreElement.textContent = +contatoreElement.textContent - 1;
-  } else {
-    // Se il valore è già 0, mostra un alert
-    alert("Il valore non può essere inferiore a 0!");
-  }
-});
+// decrementaButton.addEventListener("click", function () {
+//   // Verifico che il valore sia maggiore di 0 prima di decrementare
+//   if (+contatoreElement.textContent > 0) {
+//     // Decrementare il valore del contatore
+//     contatoreElement.textContent = +contatoreElement.textContent - 1;
+//   } else {
+//     // Se il valore è già 0, mostra un alert
+//     alert("Il valore non può essere inferiore a 0!");
+//   }
+// });
+
+// terzo esercizio:
+
+function somma() {
+  document.getElementById("valore").innerHTML;
+  document.getElementById("numberinput").value;
+
+  console.log("SOMMA");
+}
+function sottrai() {
+  console.log("SOTTRAI");
+}
+
+document.getElementById("somma").addEventListener("click", somma);
+document.getElementById("sottrai").addEventListener("click", sottrai);
+document.getElementById("numberinput").value;
